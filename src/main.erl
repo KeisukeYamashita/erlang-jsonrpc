a8bfcf89c23da1def2afc1bcaf6c509189f405cd
KeisukeYamashita <19yamashita15@gmail.com>
2018-06-19 14:56:47 +0900

    :floppy_disk: add initial test script

---
+++ src/main.erl
@@ -0,0 +1,5 @@
+-module(main).
+
+%% API
+-export([add/2]).
+add(A, B) -> A + B.
---
+++ test/erjsonrpc_test.erl
@@ -0,0 +1,7 @@
+-module(erjsonrpc_test).
+-include_lib("eunit/include/eunit.hrl").
+
+%% API
+% -export([test/0]).
+
+%% Test
